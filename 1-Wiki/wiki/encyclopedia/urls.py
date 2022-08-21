9dcfc29dbedaa9f75ef4f244f9a9a99119c6d424
Enoch <iamenochchirima@gmail.com>
2022-08-21 09:14:08 +0200

    Added New_page fucntion

--- 1-Wiki/wiki/encyclopedia/urls.py
+++ 1-Wiki/wiki/encyclopedia/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("entry_search", views.entry_search, name="entry_search"),
+    path("create_entry", views.create_entry, name="create_entry"),
+    path("<str:title>/edit", views.entries, name="edit"),
+    path("new_entry", views.new_entry, name="new_entry"),
     path("<str:title>", views.entries, name="entries")
 ]
--- 1-Wiki/wiki/encyclopedia/views.py
+++ 1-Wiki/wiki/encyclopedia/views.py
@@ -1,9 +1,22 @@
 from http.client import HTTPResponse
+from re import T
 from urllib import request
 from django.shortcuts import render, redirect
+from django import forms
 
 from . import util
 import markdown2
+
+class NewEntryForm(forms.Form):
+    title = forms.CharField(
+        label="Title",
+        widget=forms.TextInput(attrs={'class': 'form-control col-md-5 col-lg-3'})
+        )
+    content = forms.CharField(
+        label="Entry markdown content",
+        widget=forms.Textarea(attrs={'class': 'form-control col-md-8 col-lg-8'})
+        )
+    edit = forms.BooleanField(initial=False, widget=forms.HiddenInput(), required=False)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -37,4 +50,26 @@ def entry_search(request):
     "search": True,
     "value": query
     })
-    
+
+def create_entry(request):
+    return render(request, "encyclopedia/create_entry.html", {
+        "form": NewEntryForm()
+    })
+
+def new_entry(request):
+
+    if request.method == "POST":
+        form = NewEntryForm(request.POST)
+        if form.is_valid():
+
+            title = form.cleaned_data['title']
+            content = form.cleaned_data['content']
+            if util.get_entry(title) is None or form.cleaned_data['edit'] is True:
+                util.save_entry(title, content)
+                return render(request, "encyclopedia/entries.html", {
+            "title": markdown2.markdown(content),
+            "Title": title
+            })
+            else:
+                return render(request, "encyclopedia/new_entry_error.html")
+            
